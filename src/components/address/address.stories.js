054fb790f5ba9f4aa6cfd5147bd5b94ae6560dc1
DrBennemin <benjamin.krzis@gmail.com>
2020-07-24 10:56:47 +0200

    Add class location

---
+++ src/components/address/address.stories.js
@@ -0,0 +1,6 @@
+import address from "./address.html";
+import "./address.css";
+
+export default { title: "Components/Address" };
+
+export const restAddress = () => address;
--- src/components/logo/logo.stories.js
+++ src/components/logo/logo.stories.js
@@ -1,4 +1,4 @@
-import "./logo.css";
+// import "./logo.css";
 import logo from "./logo.html";
 
 export default { title: "Components/Logo" };
