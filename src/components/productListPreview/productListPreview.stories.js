f03a7473fe26c825382917592cadbffa9c1ed47f
DrBennemin <benjamin.krzis@gmail.com>
2020-07-26 11:44:20 +0200

    Split Product list into 3 component

---
+++ src/components/productListPreview/productListPreview.stories.js
@@ -0,0 +1,6 @@
+import "./productListPreview.css";
+import productListPreview from "./productListPreview.html";
+
+export default { title: "Components/ProductListPreview" };
+
+export const productListPre = () => productListPreview;
